fbc5064ecc2c10176d9627133803fd052520c2bf
anasbughio <anasbughio@gmail.com>
2026-07-21 09:59:19 +0500

    refining

--- backend/controllers/auditController.js
+++ backend/controllers/auditController.js
@@ -2,9 +2,9 @@ const AuditLog = require('../models/AuditLog');
 
 exports.getAuditLogs = async (req, res) => {
   try {
-    // Sirf us organization ke logs fetch karein jis se user belong karta hai
+    
     const logs = await AuditLog.find({ organizationId: req.user.organizationId })
-      .populate('user', 'name email') // User ka naam aur email sath layen
+      .populate('user', 'name email') 
       .sort({ createdAt: -1 }) // Sab se latest logs pehle aayenge
       .limit(100); // UI par overload se bachne ke liye limit laga di hai
 
